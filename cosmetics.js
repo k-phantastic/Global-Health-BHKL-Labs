aedb5f85beafd03976ac4f9a0325e69a04b24ae4
Khanh Phan <117617878+khp023@users.noreply.github.com>
2025-11-07 16:18:58 -0800

    Initial commit for writeup

---
+++ cosmetics.js
@@ -0,0 +1,26 @@
+// Functional code for card carousel
+document.addEventListener("DOMContentLoaded", () => {
+  const wrapper = document.querySelector(".cards-wrapper");
+  const cards = document.querySelectorAll(".card");
+  const btnNext = document.querySelector(".carousel-btn.next");
+  const btnPrev = document.querySelector(".carousel-btn.prev");
+
+  let currentIndex = 0;
+
+  const cardWidth = cards[0].offsetWidth + 20; // width + margin
+  const maxIndex = cards.length - 1;
+
+  btnNext.addEventListener("click", () => {
+    if (currentIndex < maxIndex) {
+      currentIndex += 1;
+      wrapper.style.transform = `translateX(-${cardWidth * currentIndex}px)`;
+    }
+  });
+
+  btnPrev.addEventListener("click", () => {
+    if (currentIndex > 0) {
+      currentIndex -= 1;
+      wrapper.style.transform = `translateX(-${cardWidth * currentIndex}px)`;
+    }
+  });
+});
